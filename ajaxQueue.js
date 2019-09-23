7c1a7a4a77c76f832c29345b686bcb60373f8b21
Iman Khaghani Far <iman.khaghani@gmail.com>
2019-09-23 14:21:40 -0400

    Update ajaxQueue.js

--- ajaxQueue.js
+++ ajaxQueue.js
@@ -1,32 +1,21 @@
-var AssyncQueue = function () {
+const AssyncQueue = function() {
+  var _list = [];
+  var _index = 0;
+  var _length = 0;
 
-  var _list = [], _index = 0, _length = 0;
-
-  var _callback = function () {
+  var _callback = function(params) {
     _list.shift();
     if (_list.length > 0) {
       _index++;
-      _list[0](_callback);
+      _list[0](_callback, params);
     }
   };
 
-  this.getPercentage = function () {
-    return Math.ceil((_index / _length) * 100);
-  };
-
-  this.getIndex = function () {
-    return _index;
-  };
-
-  this.getLength = function () {
-    return _length;
-  };
-
-  this.next = function (func) {
+  this.next = function(func) {
     _list.push(func);
   };
 
-  this.run = function () {
+  this.run = function() {
     if (_list.length > 0) {
       _index = 1;
       _length = _list.length;
